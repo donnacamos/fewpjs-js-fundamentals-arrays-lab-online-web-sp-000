dc537554e0fc5a70ab461ec33bd004863e33d3cc
Donna Amos <donnacamos.developer@gmail.com>
2019-10-04 21:50:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"];
+let append = ["Milo", "Otis", "Garfield"];
 append.unshift();
 
-const prepend = ["Milo", "Otis", "Garfield"];
+let prepend = ["Milo", "Otis", "Garfield"];
 prepend.push();
 
-const removeLast = ["Milo", "Otis", "Garfield"]
+let removeLast = ["Milo", "Otis", "Garfield"]
 removeLast.pop();
 
-const removeFirst = ["Milo", "Otis", "Garfield"]
+let removeFirst = ["Milo", "Otis", "Garfield"]
 removeFirst.shift(); 
